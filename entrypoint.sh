e761ffb252d4a62e66940d21d6cad35a9c456f00
odin <odinuv@gmail.com>
2019-09-04 07:48:38 +0200

    build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/sh -l
+set -e
 
-echo "hello $1"
+if [ $# -lt 4 ]
+then
+  echo "Some parameters are missing"
+  exit 1
+fi 
+APP_ID=$1
+TAG=$2
+CONFIG_ID=$3
+KBC_STORAGE_TOKEN=$4
+echo "Running configuration ${CONFIG_ID} of application ${APP_ID} using tag ${TAG}."
+
+docker pull quay.io/keboola/developer-portal-cli-v2:latest
+docker run --rm -e KBC_STORAGE_TOKEN quay.io/keboola/syrup-cli:latest run-job ${KBC_DEVELOPERPORTAL_APP} ${KBC_APP_TEST_CONFIG_ID} ${TAG}
